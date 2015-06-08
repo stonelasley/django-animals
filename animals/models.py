ac254f2639f80ce5972790e96c6b28579048f2cb
Stone C. Lasley <stonelasley@gmail.com>
2015-06-07 22:19:15 -0600

    Initial Commit

---
+++ animals/models.py
@@ -0,0 +1,136 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from livefield import LiveField, LiveManager
+
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
+class Persistable(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    featured = models.BooleanField(default=False)
+    live = LiveField()
+
+    objects = LiveManager()
+    all_objects = LiveManager(include_soft_deleted=True)
+
+    def delete(self):
+        self.live = False
+        self.save()
+
+    class Meta:
+        abstract = True
+
+    featured.boolean = True
+
+class Addressable(Persistable):
+    street = models.CharField(max_length=200)
+    block = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=175)
+    country = models.CharField(max_length=60)
+    postal_code = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+class Vet(Addressable):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Brand(Persistable):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Food(Persistable):
+    brand = models.ForeignKey(Brand)
+    name = models.CharField(max_length=200)
+    upc = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
+
+class Breed(Persistable):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Animal(Persistable):
+    name = models.CharField(max_length=255)
+    breed = models.ForeignKey(Breed)
+    birth_date = models.DateField('date of birth')
+    microchip_id = models.CharField(max_length=255, blank=True)
+    about = models.TextField()
+    private = models.BooleanField(default=False)
+
+    class Meta:
+        get_latest_by = 'created'
+        ordering = ('-created',)
+        verbose_name_plural = 'animals'
+
+    def __str__(self):
+        return self.name
+
+
+class Feeding(Persistable):
+    HR = 'Hour'
+    DY = 'Day'
+    WK = 'Week'
+    MO = 'Month'
+    EA = 'Each'
+    TSP = 'Tspn'
+    TBS = 'Tblsp'
+    CP = 'Cup'
+    ML = 'Ml'
+    GM = 'G'
+    MG = 'MG'
+
+    INTERVAL_CHOICES = (
+        (HR, 'Hour'),
+        (DY, 'Day'),
+        (WK, 'Week'),
+        (MO, 'Month'),
+    )
+
+    MEASUREMENT_CHOICES = (
+        (EA, 'Each'),
+        (TSP, 'Teaspoon'),
+        (TBS, 'Tablespoon'),
+        (CP, 'Cup'),
+        (ML, 'Milliliter'),
+        (GM, 'Gram'),
+        (MG, 'Milligram'),
+    )
+
+    interval = models.CharField(max_length=5, choices=INTERVAL_CHOICES, default=DY)
+    food = models.ForeignKey(Food)
+    alt_food = models.CharField(max_length=255, blank=True)
+    food_amount = models.DecimalField('amount', max_digits=3, decimal_places=2)
+    food_measurement = models.CharField('measurement', max_length=9, choices=MEASUREMENT_CHOICES, default=EA)
+    animal = models.ForeignKey(Animal)
+    occurrence = IntegerRangeField(min_value=0, max_value=60)
+    note = models.TextField(blank=True)
+
+    def __str__(self):
+        return "{0} {1} times per {2}".format(self.food.name, self.occurrence, self.interval)
+
+
+
+
+
---
+++ animals/tests/models_tests.py
@@ -0,0 +1,216 @@
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase
+from django.test import Client
+from django.test import TestCase, RequestFactory
+
+from animals.models import Animal, Breed, Food, Brand
+
+from django.core.urlresolvers import reverse
+
+
+def create_animal():
+    """
+    Creates an animal to test against.
+    """
+    return
+
+
+class AnimalsViewTests(TestCase):
+    """
+    View Tests
+    """
+
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.breed = Breed.objects.create(
+            name='Breed',
+            url=""
+        )
+        self.animal = Animal.objects.create(
+            breed=self.breed,
+            name="AnimalName",
+            birth_date="2001-01-01",
+            microchip_id='123456',
+            about="about",
+            private=False,
+            live=True,
+        )
+
+    def test_index_view_with_no_animals(self):
+        """
+        If no animals exist, an appropriate message should be displayed.
+        """
+        Animal.objects.all().delete()
+        response = self.client.get(reverse('animals:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No Animals")
+        self.assertQuerysetEqual(response.context['animals'], [])
+
+    def test_index_view_returns_animals(self):
+        """
+        Should return one animal
+        """
+        response = self.client.get(reverse('animals:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['animals'], ['<Animal: AnimalName>'])
+
+    def test_index_should_only_return_live_animals(self):
+        """
+        Should return one animal
+        """
+        self.animal2 = Animal.objects.create(
+            breed=self.breed,
+            name="DeadFred",
+            birth_date="2001-01-01",
+            microchip_id='123456',
+            about="about",
+            private=False,
+            live=False,
+        )
+        response = self.client.get(reverse('animals:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "DeadFread")
+        self.assertContains(response, "AnimalName")
+        self.assertEqual(len(response.context['animals']), 1)
+
+    def test_details_view_with_invalid_id(self):
+        """
+        Invalid ID should return 404
+        """
+        response = self.client.get(reverse('animals:detail', args=(9999,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_details_view_with_valid_id(self):
+        """
+        Valid ID should return 200
+        """
+        response = self.client.get(reverse('animals:detail', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_view_returns_correct_animal(self):
+        """
+        Valid ID should return corresponding animal
+        """
+        response = self.client.get(reverse('animals:detail', args=(1,)))
+        self.assertContains(response, "AnimalName")
+
+    def test_update_view_with_invalid_id(self):
+        """
+        Invalid ID should return 404
+        """
+        response = self.client.get(reverse('animals:update', args=(9999,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_view_with_valid_id(self):
+        """
+        Valid ID should return 200
+        """
+        response = self.client.get(reverse('animals:update', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_view_returns_correct_animal(self):
+        """
+        Valid ID should return corresponding animal
+        """
+        response = self.client.get(reverse('animals:update', args=(1,)))
+        self.assertContains(response, "AnimalName")
+
+    def test_delete_view_with_invalid_id(self):
+        """
+        Invalid ID should return 404
+        """
+        response = self.client.get(reverse('animals:delete', args=(9999,)))
+        self.assertEqual(response.status_code, 404)
+
+
+class AnimalsModelTests(TestCase):
+    """
+    Model Tests
+    """
+
+    def setUp(self):
+        """
+        Test Setup
+        :return:
+        """
+        self.factory = RequestFactory()
+        self.breed = Breed.objects.create(
+            name='BreedName',
+            url="http://www.google.com"
+        )
+        self.brand = Brand.objects.create(
+            name='BrandName',
+            url='http://www.google.com'
+        )
+        self.food = Food.objects.create(
+            brand=self.brand,
+            name='FoodName',
+            upc='123456'
+        )
+        self.animal = Animal.objects.create(
+            #user=self.user,
+            breed=self.breed,
+            name="AnimalName",
+            birth_date="2001-01-01",
+            microchip_id='123456',
+            about="about",
+            private=False,
+            live=True,
+        )
+
+    def test_animal_soft_delete(self):
+        """
+        Test Animal Soft Delete
+        :return: None
+        """
+        self.assertEqual(self.animal.name, "AnimalName")
+        self.assertTrue(self.animal.live)
+
+        self.animal.delete()
+
+        self.assertIsNotNone(self.animal)
+        self.assertFalse(self.animal.live)
+        self.assertEqual(self.animal.name, "AnimalName")
+
+    def test_breed_soft_delete(self):
+        """
+        Test Breed Soft Delete
+        :return: None
+        """
+        self.assertEqual(self.breed.name, "BreedName")
+        self.assertTrue(self.breed.live)
+
+        self.breed.delete()
+
+        self.assertIsNotNone(self.breed)
+        self.assertFalse(self.breed.live)
+        self.assertEqual(self.breed.name, "BreedName")
+
+    def test_food_soft_delete(self):
+        """
+        Test Food Soft Delete
+        :return: None
+        """
+        self.assertEqual(self.food.name, "FoodName")
+        self.assertTrue(self.food.live)
+
+        self.food.delete()
+
+        self.assertIsNotNone(self.food)
+        self.assertFalse(self.food.live)
+        self.assertEqual(self.food.name, "FoodName")
+
+    def test_food_soft_delete(self):
+        """
+        Test Brand Soft Delete
+        :return: None
+        """
+        self.assertEqual(self.brand.name, "BrandName")
+        self.assertTrue(self.brand.live)
+
+        self.brand.delete()
+
+        self.assertIsNotNone(self.brand)
+        self.assertFalse(self.brand.live)
+        self.assertEqual(self.brand.name, "BrandName")
