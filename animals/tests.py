751f4fbdb471133ffe85802a7c4d404d0d59d967
Stone C. Lasley <stonelasley@gmail.com>
2015-06-11 19:27:53 -0600

    simplifying structure for testing

--- animals/tests.py
+++ animals/tests.py
@@ -1,6 +1,3 @@
-from django.contrib.auth.models import AnonymousUser, User
-from django.test import TestCase
-from django.test import Client
 from django.test import TestCase, RequestFactory
 
 from animals.models import Animal, Breed, Food, Brand
