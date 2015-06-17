94ecffd6f6db4448bb53a17670afeee0ee30b70e
Stone C. Lasley <stonelasley@gmail.com>
2015-06-16 22:37:42 -0600

    Clean up admin

--- animals/admin.py
+++ animals/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+
 from animals.models import Animal, Brand, Feeding, Food, Breed, Vet
 
 class FeedingInline(admin.StackedInline):
@@ -30,19 +29,8 @@ class BrandAdmin(admin.ModelAdmin):
     fields = ['name', 'url', 'featured']
     inlines = [FoodInline]
 
-# class ProfileInline(admin.StackedInline):
-#     model = UserProfile
-#     can_delete = False
-#     verbose_name_plural = 'profile'
-
-# class UserAdmin(UserAdmin):
-#     inlines = (ProfileInline,)
-
-admin.site.unregister(User)
-
 admin.site.register(Animal, AnimalAdmin)
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Breed)
-admin.site.register(User, UserAdmin)
 admin.site.register(Vet)
 
