edc1adaee7a36802e76bd40c263297a105ba7a7e
Stone C. Lasley <stonelasley@gmail.com>
2015-06-18 21:01:08 -0600

    Lint Admin

--- animals/admin.py
+++ animals/admin.py
@@ -2,16 +2,23 @@ from django.contrib import admin
 
 from animals.models import Animal, Brand, Feeding, Food, Breed, Vet
 
+
 class FeedingInline(admin.StackedInline):
     model = Feeding
     fieldsets = [
-        ('Feed',               {'fields': ['food_amount', 'food_measurement', 'food', 'occurrence', 'interval']}),
+        ('Feed',  {'fields': ['food_amount',
+                              'food_measurement',
+                              'food',
+                              'occurrence',
+                              'interval']}),
         ('Notes', {'fields': ['note'], 'classes': ['collapse']}),
     ]
     extra = 1
 
+
 class AnimalAdmin(admin.ModelAdmin):
-    fields = ['name', 'birth_date', 'breed', 'microchip_id', 'about', 'private']
+    fields = ['name', 'birth_date', 'breed',
+              'microchip_id', 'about', 'private']
     inlines = [FeedingInline]
 
     def save_model(self, request, obj, form, change):
@@ -19,10 +26,12 @@ class AnimalAdmin(admin.ModelAdmin):
             obj.user = request.user
         obj.save()
 
+
 class FoodInline(admin.TabularInline):
     fields = ('name', 'upc', 'featured')
     model = Food
     extra = 3
+
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'featured')
@@ -33,4 +42,3 @@ admin.site.register(Animal, AnimalAdmin)
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Breed)
 admin.site.register(Vet)
-
